1b4459d27355589f487de99084d560e7580032e1
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2023-08-04 12:09:13 +0530

    Revert "Hacker House Paris"

--- src/renderer/pages/Navigation.tsx
+++ src/renderer/pages/Navigation.tsx
@@ -1,25 +1,12 @@
+import React, { ReactNode, useState } from 'react';
 import { motion } from 'framer-motion';
-import _ from 'lodash';
-import { ReactNode, useState } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import { selectRunningApps } from 'renderer/features/appsSlice';
-import {
-  deleteNode,
-  selectIsRunning,
-  setSetupComplete,
-  startNode,
-  stopNode,
-} from 'renderer/features/nodeSlice';
-import { useAppDispatch, useAppSelector } from 'renderer/utils/hooks';
 import { styled } from 'styled-components';
-import Spinner from 'renderer/components/Spinner';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { stopNode } from 'renderer/features/nodeSlice';
+import { useAppDispatch } from 'renderer/utils/hooks';
 import MadaraLogo from '../../../assets/madara-logo.png';
-import APPS_CONFIG from '../../../config/apps';
-import Apps from './Apps';
 import Logs from './Logs';
 import Telemetry from './Telemetry';
-import TwitterIcon from '../../../assets/twitter.png';
-import AppViewer from './AppViewer';
 
 const NavbarContainer = styled(motion.div)`
   background-color: black;
@@ -79,65 +66,30 @@ const NavbarItem = styled.div<{ active: boolean }>`
   border-radius: 4px;
   margin-bottom: 0.5rem;
   cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const NavbarImage = styled.img`
-  width: 1.2rem;
-  border-radius: 50%;
-  margin-right: 0.4rem;
-`;
-
-const AppSeperator = styled.hr`
-  border: 0.2px solid #282828;
-  width: 100%;
 `;
 
 type NavbarItemType = {
-  id: string;
   name: string;
   path: string;
   component: ReactNode;
 };
 const NAVBAR_ITEMS: NavbarItemType[] = [
   {
-    id: '85c147bd-32c6-433b-97f2-009167aab78b',
     name: '🔍 Logs',
     path: 'logs',
     component: <Logs />,
   },
   {
-    id: '7792b715-771b-47c2-a5c8-3c15cae16a3d',
     name: '📊 Telemetry',
     path: 'telemetery',
     component: <Telemetry />,
   },
-  {
-    id: 'fa1d9821-6fd4-46de-99ea-0f0fc967780c',
-    name: '📱 Apps',
-    path: 'apps',
-    component: <Apps />,
-  },
 ];
 
 export default function Navigtion() {
-  const [navbarActiveId, setNavbarActiveId] = useState<string>(
-    NAVBAR_ITEMS[0].id
-  );
-  const [showSpinner, setShowSpinner] = useState<boolean>(false);
+  const [navbarIndex, setNavbarIndex] = useState<number>(0);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const isNodeRunning = useAppSelector(selectIsRunning);
-  const runningApps = useAppSelector(selectRunningApps);
-
-  const handleScreenshot = async () => {
-    setShowSpinner(true);
-    await window.electron.ipcRenderer.madara.sendTweet();
-    setShowSpinner(false);
-  };
-
   return (
     <NavbarContainer>
       <Navbar
@@ -149,84 +101,25 @@ export default function Navigtion() {
           Madara
         </NavbarHeading>
         <NavbarItemsContainer>
-          {NAVBAR_ITEMS.map((item) => (
+          {NAVBAR_ITEMS.map((item, index) => (
             <NavbarItem
               onClick={() => {
                 navigate(`./${item.path}`);
-                setNavbarActiveId(item.id);
+                setNavbarIndex(index);
               }}
-              active={navbarActiveId === item.id}
+              active={navbarIndex === index}
             >
               {item.name}
             </NavbarItem>
           ))}
-          {!_.isEmpty(runningApps) && <AppSeperator />}
-          {Object.entries(runningApps)
-            .filter(([, isRunning]) => isRunning)
-            .map(([appId]) => {
-              const app = APPS_CONFIG.apps.filter((a) => a.id === appId)[0];
-              if (!app.showFrontend) {
-                return <div />;
-              }
-              return (
-                <NavbarItem
-                  onClick={() => {
-                    navigate(`./apps/${appId}`);
-                    setNavbarActiveId(appId);
-                  }}
-                  active={navbarActiveId === appId}
-                >
-                  <NavbarImage src={app.logoUrl} />
-                  {app.appName}
-                </NavbarItem>
-              );
-            })}
-          <NavbarItem
-            style={{ display: 'flex', marginTop: 'auto' }}
-            onClick={() => handleScreenshot()}
-            active={false}
-          >
-            {showSpinner ? (
-              <Spinner />
-            ) : (
-              <img
-                src={TwitterIcon}
-                width={16}
-                height={16}
-                alt="twitter icon"
-                style={{ marginRight: 10 }}
-              />
-            )}
-            <div>Did you tweet?</div>
-          </NavbarItem>
-          {isNodeRunning ? (
-            <NavbarItem
-              onClick={() => {
-                dispatch(stopNode());
-              }}
-              active={false}
-            >
-              🔌 Stop Node
-            </NavbarItem>
-          ) : (
-            <NavbarItem
-              onClick={() => {
-                dispatch(startNode());
-              }}
-              active={false}
-            >
-              ▶️ Resume Node
-            </NavbarItem>
-          )}
           <NavbarItem
             onClick={() => {
-              dispatch(deleteNode());
-              dispatch(setSetupComplete(false));
-              navigate('/');
+              dispatch(stopNode());
             }}
+            style={{ marginTop: 'auto' }}
             active={false}
           >
-            🗑️ Delete node
+            🔌 Stop Node
           </NavbarItem>
         </NavbarItemsContainer>
       </Navbar>
@@ -235,7 +128,6 @@ export default function Navigtion() {
           {NAVBAR_ITEMS.map((item) => (
             <Route path={`/${item.path}`} element={item.component} />
           ))}
-          <Route path="/apps/:appId" element={<AppViewer />} />
         </Routes>
       </ContentContainer>
     </NavbarContainer>
--- src/renderer/store/storeRegistry.ts
+++ src/renderer/store/storeRegistry.ts
@@ -1,21 +1,11 @@
 import { EnhancedStore } from '@reduxjs/toolkit';
-import { Persistor } from 'redux-persist';
 
 let store: EnhancedStore;
-let persistor: Persistor;
 
-export function registerStore(newStore: EnhancedStore) {
+export function register(newStore: EnhancedStore) {
   store = newStore;
 }
 
 export function getStore(): EnhancedStore {
   return store;
-}
-
-export function registerPersistor(newPersistor: Persistor) {
-  persistor = newPersistor;
-}
-
-export function getPersistor(): Persistor {
-  return persistor;
 }
